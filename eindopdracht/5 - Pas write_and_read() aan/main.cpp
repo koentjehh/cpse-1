7845821f654a392adf07afdf76b54aeda37a87a8
koentjehh <eikelnboomkoen@gmail.com>
2020-10-18 20:22:14 +0200

    commit

---
+++ eindopdracht/5 - Pas write_and_read() aan/main.cpp
@@ -0,0 +1,57 @@
+#include "hwlib.hpp"
+#include "spi_lib.hpp"
+
+namespace target = hwlib::target;
+
+int main( void ){
+	
+	int start;
+	int end;
+	int time;
+	
+	// wait for the terminal emulator to start up
+	hwlib::wait_ms( 2'000 );
+	hwlib::cout << "ST7789 demo\n" << hwlib::flush;   
+   
+	auto _sclk = hwlib::target::pin_out{ hwlib::target::pins::d3 };
+	auto sclk = hwlib::invert( _sclk );
+	auto mosi = hwlib::target::pin_out{ hwlib::target::pins::d4 };
+   
+	auto spi  = hwlib::spi_bit_banged_new( sclk, mosi/*, hwlib::pin_in_dummy*/ );
+      
+	auto dc    = hwlib::target::pin_out{ hwlib::target::pins::d6 };
+	auto & cs  = hwlib::pin_out_dummy;
+	auto blk   = hwlib::target::pin_out{ hwlib::target::pins::d7 };
+	auto rst   = hwlib::target::pin_out{ hwlib::target::pins::d5 };
+   
+	blk.write( 1 );blk.flush();
+
+	auto display = hwlib::st7789_spi_dc_cs_rst( spi, dc, cs, rst );   
+   
+	for(;;){
+		
+		start = hwlib::now_us();
+		
+		display.clear( hwlib::red ); 
+
+		end = hwlib::now_us();
+		time = end - start;
+		hwlib::cout << "clear() in us: " << time <<"\n";
+		
+		start = hwlib::now_us();
+		
+		display.flush();
+
+		end = hwlib::now_us();
+		time = end - start;
+		hwlib::cout << "flush() in us: " << time <<"\n";
+		
+
+
+		display.clear( hwlib::green );
+		display.flush();
+
+		display.clear( hwlib::blue );
+		display.flush();
+	}
+}
---
+++ eindopdracht/5 - Pas write_and_read() aan/spi_lib.hpp
@@ -0,0 +1,61 @@
+#include "hwlib.hpp"
+
+namespace hwlib {
+
+/// bit-banged SPI bus implementation
+///
+/// This class implements a bit-banged master interface to a SPI bus.
+class spi_bit_banged_new : public spi_bus {
+private:
+
+	pin_direct_from_out_t sclk;
+	pin_direct_from_out_t mosi;
+
+	void write_and_read( 
+		const size_t n, 
+		const uint8_t data_out[],
+		uint8_t data_in[] 
+	) override {
+
+	uint_fast8_t d = *data_out++;
+		
+	for( uint_fast8_t j = 0; j < 8; ++j ){
+		mosi.write( ( d & 0x80 ) != 0 );
+		sclk.write( 1 );
+		d = d << 1;
+			
+		sclk.write( 0 );              
+		}
+	}      
+   
+public:
+
+   /// construct a bit-banged SPI bus from the sclk, miso and mosi pins
+   ///
+   /// This constructor creates a simple bit-banged SPI bus master
+   /// from the sclk, miso and mosi pins. 
+   ///
+   /// The chip select pins for the individual chips supplied to the 
+   /// write_and_read() functions.
+   ///
+   /// When the SPI bus is used for either only writing or only reading,
+   /// the unused pin argument can be specified as pin_out_dummy or
+   /// pin_in_dummy.
+   spi_bit_banged_new( 
+      pin_out & _sclk, 
+      pin_out & _mosi/*,*/ 
+      //pin_in  & _miso 
+   ):
+      //sclk( direct( _sclk ) ), 
+      //mosi( direct( _mosi ) ), 
+      //miso( direct( _miso ) )
+      
+      sclk( _sclk ), 
+      mosi( _mosi )/*,*/ 
+      /*miso( _miso )*/   {
+      sclk.write( 0 );
+   }
+   
+}; // class spi_bit_banged_new    
+
+}; // namespace hwlib
